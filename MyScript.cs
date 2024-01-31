cedf56fd3da9e24202163599ff3e89ece62e6912
Yann <90900502+Dromach@users.noreply.github.com>
2024-01-31 15:02:45 +0100

    Add files via upload

---
+++ MyScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        float dec = 5.6f;
+        Debug.Log(dec);
+
+        string str = "ceci est un test";
+        Debug.Log(str);
+
+        short[] tab = { 2, 3, 20, 9};
+        Debug.Log(tab[1]);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
